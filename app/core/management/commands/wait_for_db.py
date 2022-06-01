945b41382a82e9b3f8adba2df04f13b0002c573c
Tim Andrews <timpandrews@yahoo.com>
2022-06-01 17:13:06 -0400

    Setup command stub & tests

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django command to wait for database to be available.
+"""
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """ wait for db """
+
+    def handle(self, *args, **options):
+        pass
