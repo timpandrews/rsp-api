39c861f20d6e2b215286a99426c162abccfc81f9
Tim Andrews <timpandrews@yahoo.com>
2022-06-01 19:21:00 -0400

    test for require email

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -31,3 +31,9 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_require_email(self):
+        """Require email for new user test raised ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
