864af64b8ea3f21a7d1655f70ad4fe8133302fb9
Tim Andrews <timpandrews@yahoo.com>
2022-06-01 22:06:47 -0400

    add create_superuser to usermanager

--- app/core/models.py
+++ app/core/models.py
@@ -18,6 +18,15 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """ create super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ Custom User Model """
