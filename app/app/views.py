6f15f691f43b87da56b07b37a0f4f9e34e598d14
Tim Andrews <timpandrews@yahoo.com>
2022-06-06 13:41:12 -0400

    add link to home page

--- app/app/views.py
+++ app/app/views.py
@@ -4,5 +4,9 @@ import datetime
 
 def home(request):
     now = datetime.datetime.now()
-    html = "<html><body><h1>RSP-API</h1>It is now %s.</body></html>" % now
+    html = "<html><body>"
+    html += "<h1>RSP-API</h1>"
+    html += "It is now %s." % now
+    html += "<br><br><div><a href='/api/docs/'>Schema</a><div>"
+    html += "</body></html>"
     return HttpResponse(html)
