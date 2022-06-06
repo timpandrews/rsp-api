d7de8cb9d65fdcf69cc586a26fc4c1c5de341707
Tim Andrews <timpandrews@yahoo.com>
2022-06-06 13:30:09 -0400

    get token api

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party apps
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     'drf_spectacular',
     # local apps
     'core',
