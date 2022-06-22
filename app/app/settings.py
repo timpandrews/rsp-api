9a72f1c85db6e4b3577dde831fb234775967d3c6
Tim Andrews <timpandrews@yahoo.com>
2022-06-22 11:10:39 -0400

    config settings.py to use values from .env

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ykk*kej7cc^=d)0))n=)q9+w6eknv5f#k6s&yd-g%b)r^y07^e'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'uedj3changeme1351!@#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
